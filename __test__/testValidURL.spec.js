2282e99df5bd4680b9c9815c458fba084e925cf7
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-02-26 00:37:29 -0500

    :fire: Remove Files: Remove unused files

--- __test__/testValidURL.spec.js
+++
@@ -1,8 +0,0 @@
-import { validURL } from "../src/client/js/validURL";
-
-// To check if 'validURL' produces the right output.
-describe("Testing the submit functionality", () => {
-  test("Testing the handleSubmit() function", () => {
-    expect(validURL).toBeDefined();
-  });
-});
