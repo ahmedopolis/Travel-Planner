8b0028c460fa42e67562cd76a4b1764c6610746c
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-03-30 01:14:29 -0400

    :seedling: Update Seeds: Change seeds

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,9 @@
 import { runAction } from "./js/formHandler";
 
 import "./styles/main.scss";
-//import "./styles/resets.scss";
 import "./styles/footer.scss";
 import "./styles/header.scss";
 import "./styles/form.scss";
+import "./styles/results.scss";
 
 export { runAction };
