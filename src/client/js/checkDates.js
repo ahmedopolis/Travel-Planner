f49e479a166fb96543d0c592cc5b3745abb334ea
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-04-08 18:20:23 -0400

    :art: Improve Structure: Extend conditional statements

--- src/client/js/checkDates.js
+++ src/client/js/checkDates.js
@@ -3,7 +3,7 @@ function checkOrderDate(today, date1, date2) {
   const currentDate = new Date(today);
   const newDate1 = new Date(date1);
   const newDate2 = new Date(date2);
-  if (newDate1 < currentDate || newDate2 < currentDate) {
+  if (newDate1 < currentDate || newDate2 < currentDate || newDate2 < newDate1) {
     alert(
       "The entry dates are dated before the current date, or the start date is set later than the end date."
     );
